3ceaf5dd720c04fa9c7d183260220aa9f597abe4
Ramiro Ivan Flappo <rflappo@Ramiros-Mac-mini.local>
2025-10-19 23:26:46 -0300

    Adding 3 units to show

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 UNITS = []
 # This should be a database in the future
-with open("static/data/models2.json", encoding="utf-8") as data_file:
+with open("static/data/models.json", encoding="utf-8") as data_file:
     UNITS = load(data_file)
 
 # Serve /static/*
